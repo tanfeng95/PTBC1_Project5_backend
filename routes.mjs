5a9a812fc3088648e7b7899fecc4e36c646cc245
tanfeng95 <tanfeng95@hotmail.com>
2022-07-05 07:31:48 +0800

    add order controller

--- routes.mjs
+++ routes.mjs
@@ -3,13 +3,17 @@ import db from './models/index.mjs';
 // import your controllers here
 import initProductsController from './controllers/productController.mjs';
 import initUserController from './controllers/userController.mjs';
+import initOrdersController from './controllers/orderController.mjs';
 
 export default function bindRoutes(app) {
   const productController = initProductsController(db);
   const userController = initUserController(db);
+  const orderController = initOrdersController(db);
 
   app.get('/products', productController.FindAllProduct);
   app.get('/product/:id', productController.getProductById);
   app.post('/login', userController.login);
   app.post('/signup', userController.signup);
+
+  app.post('/createOrder', orderController.addOrder);
 }
