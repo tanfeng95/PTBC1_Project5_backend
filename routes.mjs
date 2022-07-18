0d8dfd1465ce61a0778c8120abcfd4a78535534e
makck <joel.mak32@gmail.com>
2022-07-18 12:31:26 +0800

    Add multer config and multerUpload in endpoints

--- routes.mjs
+++ routes.mjs
@@ -1,3 +1,4 @@
+import multer from 'multer';
 import db from './models/index.mjs';
 
 // import your controllers here
@@ -9,6 +10,19 @@ export default function bindRoutes(app) {
   const productController = initProductsController(db);
   const userController = initUserController(db);
   const orderController = initOrdersController(db);
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+      cb(null, `${file.fieldname}-${Date.now()}`);
+    },
+  });
+
+  const multerUpload = multer({
+    storage,
+  });
 
   app.get('/products', productController.FindAllProduct);
   app.get('/product/:id', productController.getProductById);
@@ -22,7 +36,7 @@ export default function bindRoutes(app) {
   app.get('/merchant/dashboard/:id', orderController.getOrderByMerchantId);
   app.get('/merchant/:id', productController.getProductByMerchantId);
   app.put('/merchant/product/delete/:merchantId/:productId', productController.deleteProductById);
-  app.post('/merchant/product/add/:merchantId', productController.addProduct);
+  app.post('/merchant/product/add/:merchantId', multerUpload.single('image'), productController.addProduct);
   app.get('/merchant/product/edit/:merchantId/:productId', productController.getProductByMerchantIdByProductId);
-  app.put('/merchant/product/edit/:merchantId/:productId', productController.editProduct);
+  app.put('/merchant/product/edit/:merchantId/:productId', multerUpload.single('image'), productController.editProduct);
 }
